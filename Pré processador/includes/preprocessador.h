293c5f7b210f05f5a0c620b199005b841c8eeb0d
Avkkv <kaua58471@gmail.com>
2026-08-29 17:31:16 -0300

    pastas para o pré processador

---
+++ Pré processador/includes/preprocessador.h
@@ -0,0 +1,19 @@
+#ifndef preprocessador
+#define preprocessador
+
+
+
+
+#endif
+
+/*
+
+Referências:
+ - https://www.mentebinaria.com.br/artigos/programa%C3%A7%C3%A3o/construindo-seu-pr%C3%B3prio-compilador-parte-1-r58/
+ 
+
+
+
+
+
+*/
---
+++ Pré processador/includes/src/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include"preprocessador.h"
+
+int main(int argc, char* argv[]){
+    
+
+}
