72631617807c6e67667e5262ad87a6c7f974d6bc
Alexandre Vasconcelos Vicente <alexandre.vic007@gmail.com>
2026-09-03 18:16:47 -0300

    Implementa funcao + header e realiza teste no main.c

--- Pré processador/src/main.c
+++ Pré processador/src/main.c
@@ -3,7 +3,53 @@
 #include<stdlib.h>
 #include"preprocessador.h"
 
-int main(int argc, char* argv[]){
-    
+int main(int argc, char *argv[]) {
 
+    FILE * arquivo_entrada;
+    FILE * arquivo_saida;
+
+    /* Verifica se foram informados exatamente dois argumentos */
+    if (argc != 3) {
+
+        fprintf(stderr,
+                "Uso: %s <arquivo_entrada.asm> <arquivo_saida.pre>\n",
+                argv[0]);
+
+        return 1;
+    }
+
+    /* Abre o arquivo de entrada para leitura */
+    arquivo_entrada = fopen(argv[1], "r");
+
+    if (arquivo_entrada == NULL) {
+
+        fprintf(stderr,
+                "Erro: nao foi possivel abrir o arquivo de entrada '%s'.\n",
+                argv[1]);
+
+        return 1;
+    }
+
+    /* Abre o arquivo de saida para escrita */
+    arquivo_saida = fopen(argv[2], "w");
+
+    if (arquivo_saida == NULL) {
+        
+        fprintf(stderr,
+                "Erro: nao foi possivel criar o arquivo de saida '%s'.\n",
+                argv[2]);
+
+        fclose(arquivo_entrada);
+
+        return 1;
+    }
+
+    /* Executa o pre-processamento */
+    preprocessar(arquivo_entrada, arquivo_saida);
+
+    /* Fecha os arquivos */
+    fclose(arquivo_entrada);
+    fclose(arquivo_saida);
+
+    return 0;
 }
--- Pré processador/src/preprocessador.c
+++ Pré processador/src/preprocessador.c
@@ -1,4 +1,90 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include"preprocessador.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "preprocessador.h"
+
+void remover_comentario(const char * entrada, char * saida) {
+
+    int dentro_str = 0;
+    int i = 0;
+    int j = 0;
+
+    while (entrada[i] != '\0') {
+        char c = entrada[i];
+
+        if (c == '"') {
+            dentro_str = !dentro_str;
+            saida[j++] = c;
+        }
+        else if (c == '#' && !dentro_str) {
+            break;
+        }
+        else {
+            saida[j++] = c;
+        }
+
+        i++;
+    }
+
+    saida[j] = '\0';
+}
+
+void normalizar_espacos(const char * entrada, char * saida) {
+
+    int dentro_str = 0;
+    int i = 0;
+    int j = 0;
+    int tem_conteudo = 0;
+
+    while (entrada[i] != '\0') {
+
+        char c = entrada[i];
+
+        if (c == '"') {
+            dentro_str = !dentro_str;
+            saida[j++] = c;
+            tem_conteudo = 1;
+        }
+        else if (dentro_str) {
+            saida[j++] = c;
+            tem_conteudo = 1;
+        }
+        else if (c == ' ' || c == '\t') {
+            if (tem_conteudo && j > 0 && saida[j - 1] != ' ') {
+                saida[j++] = ' ';
+            }
+        }
+        else if (c != '\r' && c != '\n') {
+            saida[j++] = c;
+            tem_conteudo = 1;
+        }
+
+        i++;
+    }
+
+    while (j > 0 && saida[j - 1] == ' ') j--;
+   
+    saida[j] = '\0';
+}
+
+void processar_linha(const char *linha, FILE *saida) {
+
+    char sem_comentario[TAM_LINHA];
+    char normalizada[TAM_LINHA];
+
+    remover_comentario(linha, sem_comentario);
+    normalizar_espacos(sem_comentario, normalizada);
+
+    if (normalizada[0] != '\0') {
+        fprintf(saida, "%s\n", normalizada);
+    }
+}
+
+void preprocessar(FILE * entrada, FILE * saida)
+{
+    char linha[TAM_LINHA];
+
+    while (fgets(linha, TAM_LINHA, entrada) != NULL) {
+        processar_linha(linha, saida);
+    }
+}
