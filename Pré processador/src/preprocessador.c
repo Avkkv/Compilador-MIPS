f1b09c9421dfd9de0f0dface11b3daf6e831ecbf
Avkkv <kaua58471@gmail.com>
2026-09-07 18:54:41 -0300

    Update preprocessador.c

--- Pré processador/src/preprocessador.c
+++ Pré processador/src/preprocessador.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "preprocessador.h"
+#include"../includes/preprocessador.h"
 
 void remover_comentario(const char * entrada, char * saida) {
 
